417820a61dc2318cb04f38b6ad43f8bb26bc2c07
neerajmaurya1910 <neerajmaurya1910@gmail.com>
2026-09-10 09:22:46 +0530

    Refactor Student class to include main method
    Removed instance variables and methods from Student class.

--- Student.java
+++ Student.java
@@ -1,29 +1,10 @@
 public class Student {
-    private int id;
-    private String name;
-    private double marks;
 
-    public Student(int id, String name, double marks) {
-        this.id = id;
-        this.name = name;
-        this.marks = marks;
-    }
+    public static void main(String[] args) {
+        String name = "Neeraj";
+        int age = 21;
 
-    public int getId() {
-        return id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public double getMarks() {
-        return marks;
-    }
-
-    public void display() {
-        System.out.println("ID: " + id +
-                " | Name: " + name +
-                " | Marks: " + marks);
+        System.out.println("Student Name: " + name);
+        System.out.println("Student Age: " + age);
     }
 }
